098e974932baa4575beb4bb0582bebaaca24a6af
feyzayilmaz <efeyzyum@gmail.com>
2023-12-07 12:49:16 +0300

    Proje klasörleri oluşturuldu. Entity ve contex yazıldı.

---
+++ humanResourceProject.Infrastructure/Context/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using humanResourceProject.Domain.Entities;
+using humanResourceProject.Infrastructure.EntityTypeConfig;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanResourceProject.Infrastructure.Context
+{
+    public class AppDbContext:IdentityDbContext<AppUser, IdentityRole<Guid>, Guid>
+    { 
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<CompanyInformation> CompanyDetails { get; set; }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.ApplyConfiguration(new AppUserConfig());
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ humanResourceProject.Infrastructure/EntityTypeConfig/AppUserConfig.cs
@@ -0,0 +1,23 @@
+﻿using humanResourceProject.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanResourceProject.Infrastructure.EntityTypeConfig
+{
+    internal class AppUserConfig: BaseEntityConfig<AppUser>
+    {
+        
+        
+        public override void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.HasOne(x=>x.companyInformation).WithMany(x=>x.Employees).HasForeignKey(x=>x.CompanyId);
+          
+            base.Configure(builder);
+        }
+    }
+}
