c9f6db3625819cfee26b14d1c1d1bf9f0e4ba38e
feyzayilmaz <efeyzyum@gmail.com>
2023-12-08 10:55:55 +0300

    DTO katmanı oluşturuldu

---
+++ humanResourceProject.DTO/DTOs/RegisterDTO.cs
@@ -0,0 +1,31 @@
+﻿using humanResourceProject.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace humanResourceProject.DTO.DTOs
+{
+    public class RegisterDTO
+    {
+        [Display(Name = "Kullanıcı Adı")]
+        [Required(ErrorMessage = "Kullanıcı adı boş olamaz")]
+        [MaxLength(50)]
+        [MinLength(3, ErrorMessage = "Kullanıcı Adı 3 karakterden az olamaz")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre alanı boş olamaz")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        public string ConfirmedPassword { get; set; }
+
+        public string UserRole { get; set; } = "User";
+        public string Email { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        public Status status => Status.Active;
+    }
+}
