73381aaa6d2584637a4c0a215f80438ae94a0fe4
Can Tokhay <cantokhay@gmail.com>
2023-12-08 17:35:51 +0300

    AppUserVM and CompanyVM has been created in the new Layer of VM

---
+++ humanResourceProject.VM/VMs/CompanyVM.cs
@@ -0,0 +1,13 @@
+﻿namespace humanResourceProject.VM.VMs
+{
+    public class CompanyVM
+    {
+        public Guid Id { get; set; }
+        public string CompanyName { get; set; }
+        public int NumberOfEmployees { get; set; }
+        public string TaxNumber { get; set; }
+        public string TaxOffice { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
