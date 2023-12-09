5cce237de0f54c6ac216544b631fab7d25734479
feyzayilmaz <efeyzyum@gmail.com>
2023-12-09 20:05:53 +0300

    Servise Ekleme.

---
+++ humanResourceProject.Models/DTOs/UpdateUserDTO.cs
@@ -0,0 +1,21 @@
+﻿using humanResourceProject.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace humanResourceProject.Models.DTOs
+{
+    public class UpdateUserDTO
+    {
+        public string Email { get; set; }
+        public string LastName { get; set; }
+        public string Adress { get; set; }
+        public string PhoneNumber { get; set; }
+        public Title Title { get; set; }
+        public string Job { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public Status status { get; set; }
+    }
+}
---
+++ humanResourceProject.Presentation/Controllers/CompanyController.cs
@@ -0,0 +1,15 @@
+﻿using humanResourceProject.Application.Services.Abstract.ICompanyServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace humanResourceProject.Presentation.Controllers
+{
+    public class CompanyController : Controller
+    {
+        private readonly ICompanyWriteService _writeCompanyService;
+        private readonly ICompanyReadService _readService;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
