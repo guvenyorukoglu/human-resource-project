ca281488ec26301163b22f00809dba851ade595a
UTolga <ulutas.tolga@outlook.com>
2023-12-10 22:53:35 +0300

    Presentation-Çalışanların Listelenmesi

--- humanResourceProject.Presentation/Controllers/ManagerController.cs
+++ humanResourceProject.Presentation/Controllers/ManagerController.cs
@@ -1,20 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using humanResourceProject.Domain.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
 
 namespace humanResourceProject.Presentation.Controllers
 {
     public class ManagerController : Controller
     {
-        
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-        public IActionResult Index()
+        public ManagerController(IConfiguration configuration)
         {
-            return View();
+            _configuration = configuration;
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new Uri("https://localhost:7255/");
+
         }
 
-        public IActionResult Employees()
-        {
 
-            return View();
+        public async Task<IActionResult> Employees()
+        {
+                   
+           
+            var response = await _httpClient.GetAsync("api/AppUser");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var employees = JsonConvert.DeserializeObject<List<AppUser>>(content);
+                return View(employees);
+
+            }
+            return View("Error");
         }
+
     }
 }
