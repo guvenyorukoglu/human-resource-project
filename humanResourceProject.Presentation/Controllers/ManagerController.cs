7f3dcfab15bcafb00f2bdf997439395ba8861b29
UTolga <ulutas.tolga@outlook.com>
2023-12-11 13:15:20 +0300

    Presentation Layer>ManagerController>EditEmployee ve DeleteEmployee metotları üzerine çalışma devam ediyor

--- humanResourceProject.Presentation/Controllers/ManagerController.cs
+++ humanResourceProject.Presentation/Controllers/ManagerController.cs
@@ -1,7 +1,9 @@
-﻿using humanResourceProject.Domain.Entities.Concrete;
+﻿using Bogus.Bson;
+using humanResourceProject.Domain.Entities.Concrete;
+using humanResourceProject.Models.DTOs;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
+using System.Text;
 
 namespace humanResourceProject.Presentation.Controllers
 {
@@ -33,6 +35,61 @@ namespace humanResourceProject.Presentation.Controllers
             }
             return View("Error");
         }
+
+
+        
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteEmployee(Guid id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/AppUser/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                
+                return RedirectToAction("Employees"); 
+            }
+
+            return View("Error");
+        }
+
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> EditEmployee(Guid id)
+        {
+            var response = await _httpClient.GetAsync($"api/AppUser/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var employee = JsonConvert.DeserializeObject<UpdateUserDTO>(content);
+                return View(employee);
+
+            }
+            return View("Error");
+
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditEmployee(Guid id, UpdateUserDTO updateUserDTO)
+        {
+           
+            var json = JsonConvert.SerializeObject(updateUserDTO);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PutAsync($"api/AppUser/{id}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Employees");
+            }
+
+            return View("Error");
+        }
+
+
 
     }
 }
