01e0fdafb501feeca37da9197e8980856f1cae78
Güven Yörükoğlu <yorukoglu.guven@gmail.com>
2023-12-14 19:08:08 +0300

    RegularExpression'a Türkçe karakterlerde eklendi

--- humanResourceProject.API/Program.cs
+++ humanResourceProject.API/Program.cs
@@ -20,8 +20,8 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    //options.UseSqlServer(builder.Configuration.GetConnectionString("MonitorEase:SqlDb"));   //Azure
-    options.UseSqlServer(builder.Configuration.GetConnectionString("GuvenSQLConnection")); //Local
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MonitorEase:SqlDb"));   //Azure
+    //options.UseSqlServer(builder.Configuration.GetConnectionString("GuvenSQLConnection")); //Local
 
 
 });
