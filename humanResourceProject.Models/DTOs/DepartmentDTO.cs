935f99a40a977c873b67120e310374c8844be872
Can Tokhay <cantokhay@gmail.com>
2023-12-19 13:13:25 +0300

    data annotations

--- humanResourceProject.Models/DTOs/DepartmentDTO.cs
+++ humanResourceProject.Models/DTOs/DepartmentDTO.cs
@@ -1,11 +1,19 @@
 ﻿using humanResourceProject.Domain.Enum;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace humanResourceProject.Models.DTOs
 {
     public class DepartmentDTO
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Departman adı alanı boş geçilemez!")]
+        [DisplayName("Departman adı*")]
+        [RegularExpression(@"^[a-zA-ZğĞıİşŞüÜöÖçÇ]*$", ErrorMessage = "Yalnızca alfabetik karakterlere izin verilir.")]
         public string DepartmentName { get; set; }
+        [Required(ErrorMessage = "Açıklama alanı boş geçilemez!")]
+        [DisplayName("Açıklama*")]
+        [StringLength(500, ErrorMessage = "Açıklama en fazla 500 en az 5 karakter olmalıdır.", MinimumLength = 5)]
         public string? Description { get; set; }
         public Status Status { get; set; }
         public DateTime? UpdateDate { get; set; }
