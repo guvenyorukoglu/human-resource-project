f00434afdc72415e498c7ded75633da26aaf7b8e
Can Tokhay <cantokhay@gmail.com>
2023-12-20 09:04:21 +0300

    commented out for testing

--- humanResourceProject.Presentation/Program.cs
+++ humanResourceProject.Presentation/Program.cs
@@ -26,6 +26,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+//commented out for testing
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
