ba56ca99f6c0586d60f90c17c1b5889b3368c913
feyzayilmaz <efeyzyum@gmail.com>
2024-01-03 09:49:33 +0300

    UpdateProfileEmployee Tek  Sayfada yaplıyor

--- humanResourceProject.Models/VMs/ProfileEmployeeVM.cs
+++ humanResourceProject.Models/VMs/ProfileEmployeeVM.cs
@@ -11,8 +11,8 @@ namespace humanResourceProject.Models.VMs
     {
         public Guid Id { get; set; }
         public string FullName { get; set; }
-        public string? Address { get; set; }
-        public string? PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
         public string? Email { get; set; }
         public string? JobTitle { get; set; }
         public string IdentificationNumber { get; set; }
