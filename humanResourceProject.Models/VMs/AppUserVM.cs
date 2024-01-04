b461a5a4f46b1a50a859d347661dc23182666fc5
UTolga <ulutas.tolga@outlook.com>
2024-01-04 09:23:44 +0300

    Frontend Fixes

--- humanResourceProject.Models/VMs/AppUserVM.cs
+++ humanResourceProject.Models/VMs/AppUserVM.cs
@@ -1,4 +1,6 @@
-﻿namespace humanResourceProject.Models.VMs
+﻿using humanResourceProject.Domain.Enum;
+
+namespace humanResourceProject.Models.VMs
 {
     public class AppUserVM
     {
@@ -7,7 +9,7 @@
         public string Email { get; set; }
         public string Address { get; set; }
         public string PhoneNumber { get; set; }
-
+        public Gender Gender { get; set; }
         public decimal EarnedLeaveDays { get; set; }
         public decimal RemainingLeaveDays { get; set; }
     }
