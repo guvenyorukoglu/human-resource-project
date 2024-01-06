da2ed9389225e35db7cc964487635ed4f62c247a
Güven Yörükoğlu <yorukoglu.guven@gmail.com>
2024-01-06 19:59:52 +0300

    15-Liste Başlıkları standartlaştırıldı, margin eklendi

--- humanResourceProject.Models/DTOs/UpdateCompanyDTO.cs
+++ humanResourceProject.Models/DTOs/UpdateCompanyDTO.cs
@@ -1,35 +1,29 @@
 using humanResourceProject.Domain.Enum;
 using humanResourceProject.Models.Validations;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace humanResourceProject.Models.DTOs
 {
     public class UpdateCompanyDTO
     {
-
         public Guid Id { get; set; }
 
         [Required(ErrorMessage = "Şirket Adı alanı boş geçilemez!")]
-        [DisplayName("Şirket Adı")]
+        [DisplayName("Şirket Adı*")]
         [StringLength(100, ErrorMessage = "Şirket adı en fazla 100 en az 5 karakter olmalıdır.", MinimumLength = 5)]
         public string CompanyName { get; set; }
         [Required(ErrorMessage = "Adres alanı boş geçilemez!")]
-        [DisplayName("Adres")]
+        [DisplayName("Adres*")]
         [StringLength(200, ErrorMessage = "Adres en fazla 200 en az 5 karakter olmalıdır.", MinimumLength = 5)]
         public string Address { get; set; }
         [Required(ErrorMessage = "Telefonu alanı boş geçilemez!")]
-        [DisplayName("Telefon")]
+        [DisplayName("Telefon*")]
         [PhoneValidations(ErrorMessage = "Lütfen geçerli bir telefon numarası giriniz!")]
         public string PhoneNumber { get; set; }
         [Required(ErrorMessage = "Personel sayısı alanı boş geçilemez!")]
-        [DisplayName("Personel Sayısı")]
+        [DisplayName("Personel Sayısı*")]
         public int NumberOfEmployees { get; set; }
         public DateTime? UpdateDate { get; set; }
         public Status Status { get; set; }
